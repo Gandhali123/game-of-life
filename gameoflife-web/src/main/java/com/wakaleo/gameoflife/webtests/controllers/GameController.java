ca7d6a96e8976ba4b29d70b35df75d16bf510406
Gandhali123 <42291254+Gandhali123@users.noreply.github.com>
2020-05-02 17:18:45 +0530

    new comment added

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -14,7 +14,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  /**
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
- *
+ * new comment added and commit
  * GameController.java
  * Controller to navigate the buttons on the game pages
  */
